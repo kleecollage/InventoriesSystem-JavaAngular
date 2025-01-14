7da03812b18c492e14bc3572dcc593ddedfc689e
Klee Collage <klee.collage@gmail.com>
2025-01-13 21:37:05 -0600

    feat(model): Create Product Model with JPA annotations

---
+++ src/main/java/gm/inventories/model/Product.java
@@ -0,0 +1,83 @@
+package gm.inventories.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+// @ToString
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idProduct;
+    String descriptionProduct;
+    Double priceProduct;
+    Integer stock;
+
+    public Integer getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(Integer idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getDescriptionProduct() {
+        return descriptionProduct;
+    }
+
+    public void setDescriptionProduct(String descriptionProduct) {
+        this.descriptionProduct = descriptionProduct;
+    }
+
+    public Double getPriceProduct() {
+        return priceProduct;
+    }
+
+    public void setPriceProduct(Double priceProduct) {
+        this.priceProduct = priceProduct;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "idProduct=" + idProduct +
+                ", descriptionProduct='" + descriptionProduct + '\'' +
+                ", priceProduct=" + priceProduct +
+                ", stock=" + stock +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

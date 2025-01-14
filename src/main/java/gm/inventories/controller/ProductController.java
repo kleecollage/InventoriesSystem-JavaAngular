6b07f7e7961a15065a57531bdf0ea5dfd021885a
Klee Collage <klee.collage@gmail.com>
2025-01-13 22:30:43 -0600

    feat(controller): Implement getProducts method

---
+++ src/main/java/gm/inventories/controller/ProductController.java
@@ -0,0 +1,47 @@
+package gm.inventories.controller;
+
+import gm.inventories.model.Product;
+import gm.inventories.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+// http://localhost:8080/inventory-app
+@RequestMapping("inventory-app")
+@CrossOrigin(value = "http://localhost:4200")
+public class ProductController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    private final ProductService productService;
+    //CONSTRUCTOR
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // GET [host/inventory-app/products]
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        List<Product> products = productService.listProducts();
+        logger.info("Products list: ");
+        // DEBUG
+        // products.forEach(product -> logger.info(product.toString()));
+        return products;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+

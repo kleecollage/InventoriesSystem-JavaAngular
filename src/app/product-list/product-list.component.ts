2c225ed33840d77780e75ca515254e988fd2b958
Klee Collage <klee.collage@gmail.com>
2025-01-15 19:56:26 -0600

    feat(component): Add product-list template

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: false,
+  
+  templateUrl: './product-list.component.html',
+  styles: ``
+})
+export class ProductListComponent {
+
+}
